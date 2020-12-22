569dc546514ac1d25addf96a225342a33e5667ef
AnnaPresciuttini <56429403+AnnaPresciuttini@users.noreply.github.com>
2020-12-22 15:15:12 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -39,7 +39,7 @@ def load_data(nrows):
     return data
 
 data_load_state = st.text('Loading data...')
-data = load_data(200)
+data = load_data(5994)
 data_ = data.rename(columns={'Date':'index'}).set_index('index')
 
 
