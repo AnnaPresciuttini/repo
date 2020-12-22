c372a7546e6da228dcda55c63cd888265804e5d8
AnnaPresciuttini <56429403+AnnaPresciuttini@users.noreply.github.com>
2020-12-22 18:17:34 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -189,14 +189,7 @@ if not st.sidebar.checkbox("Hide", True, key='1'):
          df = px.data.tips()
          fig = px.box(df, y="high")
          fig.show()
-    if select_2 == 'Low':
-         st.write(seaborn.boxplot(data.low)
-    if select_2 == 'Close':
-         st.write(seaborn.boxplot(data.close)
-    if select_2 == 'Volume':
-         st.write(seaborn.boxplot(data.volume)
-    if select_2 == 'Weekly returns':
-         st.write(seaborn.boxplot(data.weekly_returns)
+
     
     
     
