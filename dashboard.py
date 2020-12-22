f7d367eef66369f8bef8413387e509c9f8dec0df
AnnaPresciuttini <56429403+AnnaPresciuttini@users.noreply.github.com>
2020-12-22 14:40:28 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -48,8 +48,12 @@ st.dataframe(data.head(500))
 
 if not st.sidebar.checkbox("Hide", True, key='1'):
     if select == 'Adjusted Closing Prices':
-        fig = px.line(data, x=data['Date'], y=data.adjclose, title='Life expectancy in Canada')
-        fig.show()
+        data = data.loc[data['Date'] >= '1999-1-01']
+        data.plot(x='Date', y= 'adjclose')
+        plt.xlabel("Date")
+        plt.ylabel( 'Adjusted Closing prices')
+        plt.title( 'Italian adjusted closing prices history')
+        plt.legend().set_visible(False)
         
 
  
