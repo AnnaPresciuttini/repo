8e937659a1878736468e755a9bca8981e8fb6e01
AnnaPresciuttini <56429403+AnnaPresciuttini@users.noreply.github.com>
2020-12-22 16:23:21 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -88,6 +88,21 @@ if not st.sidebar.checkbox("Hide", True, key='1'):
         st.line_chart(data_.volume)
     if select == 'Weekly returns':
         st.line_chart(data_.weekly_returns)
+        
+    if select_2 == 'Adjusted Closing Prices':
+        st.write(seaborn.boxplot(data.adjclose)
+    if select_2 == 'Open':
+         st.write(seaborn.boxplot(data.open)
+    if select_2 == 'High':
+         st.write(seaborn.boxplot(data.high)
+    if select_2 == 'Low':
+         st.write(seaborn.boxplot(data.low)
+    if select_2 == 'Close':
+         st.write(seaborn.boxplot(data.close)
+    if select_2 == 'Volume':
+         st.write(seaborn.boxplot(data.volume)
+    if select_2 == 'Weekly returns':
+         st.write(seaborn.boxplot(data.weekly_returns)
     
     
     
