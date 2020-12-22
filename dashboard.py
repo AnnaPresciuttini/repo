45a04f05bd038b303d094fc5bfb7a8b0b25d1543
AnnaPresciuttini <56429403+AnnaPresciuttini@users.noreply.github.com>
2020-12-22 17:15:34 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -84,7 +84,7 @@ if st.checkbox("Show distribution of the features"):
             st.write("### Heatmap")
             data[['high', 'low', 'open', 'close', 'volume', 'adjclose', 'weekly_returns']].hist(bins=15, figsize=(15, 6), layout=(2, 4), color='lightblue', grid=False)
             plt.show()
-            st.pyplot()
+            
 
 
 data_ = data.rename(columns={'Date':'index'}).set_index('index')
