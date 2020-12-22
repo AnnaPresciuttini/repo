6b320bdc69d99bf67ec5f972d7381e7ecad4fae4
AnnaPresciuttini <56429403+AnnaPresciuttini@users.noreply.github.com>
2020-12-22 15:53:50 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -31,7 +31,7 @@ import pandas as pd
 st.title('Predicting weekly returns of the FTSE MIB Index')
 
 st.sidebar.title("Exploratory data analysis:")
-select = st.sidebar.selectbox('Feature', ['Adjusted Closing Prices', 'Open', 'High','Volume', 'Weekly returns',], key='1')
+select = st.sidebar.selectbox('Feature', ['Adjusted Closing Prices', 'Open', 'High','Volume', 'Low', 'Close', 'Weekly returns',], key='1')
 
 
 @st.cache
@@ -56,6 +56,19 @@ data_ = data.rename(columns={'Date':'index'}).set_index('index')
 if not st.sidebar.checkbox("Hide", True, key='1'):
     if select == 'Adjusted Closing Prices':
         st.line_chart(data_.adjclose)
+    if select == 'Open':
+        st.line_chart(data_.open)
+     if select == 'High':
+        st.line_chart(data_.high)
+    if select == 'Low':
+        st.line_chart(data_.low)
+    if select == 'Close':
+        st.line_chart(data_.close)
+    if select == 'Weekly returns':
+        st.line_chart(data_.weekly_returns)
+    
+    
+    
         
 
  
